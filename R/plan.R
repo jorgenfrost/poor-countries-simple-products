ec89643f90873e70e3c886e8dec45f12b983cdb2
soerenpost <sorenpost@protonmail.com>
2020-07-15 13:45:36 +0200

    added plant_tbl

--- R/plan.R
+++ R/plan.R
@@ -2,6 +2,7 @@ the_plan <-
   drake::drake_plan(
     
     # Clean miscellaneous data --------------------------------------
+
     wpi_tbl = clean_wpi(
       wpi_1993_94_path = file_in("data/external/deflators/wpi_eaindustry_nic/wpi_base_1993_94.xls"),
       wpi_2004_05_path = file_in("data/external/deflators/wpi_eaindustry_nic/wpi_base_2004_05.xls"),
@@ -12,8 +13,12 @@ the_plan <-
       min_year = 1998
     ),
     # TODO: Clean enterprise surveys
+    # TODO: Clean NSS data
+    # TODO: Clean RBI data
+    # TODO: Clean rainfall data
+    # TODO: Clean temperature data
     
-    # Read, clean ASI data and get base sample ----------------------
+    # Read and clean ASI data  --------------------------------------
     
     # Clean blocks
     npcms_blocks_raw = read_npcms_blocks(),
@@ -22,9 +27,10 @@ the_plan <-
     ),
     
     # Get plant-level data (one obs per plant)
-    # TODO:           plant_tbl = get_plant_tbl(
-    #                   block_list = asi_blocks_clean
-    #                   )
+    plant_ls = get_plant_tbl(
+                       block_list = asi_blocks_clean,
+		       wpi_index = wpi_tbl
+		       ),
     
     # Get plant complexity ------------------------------------------
     
@@ -32,7 +38,7 @@ the_plan <-
       export_data = file_in("data/external/international_trade/year_origin_hs96_4.tsv"),
       pop_data = pop_tbl
     ),
-    rca_tbl = get_comparative_advantage(
+    rca_tbl = get_comparative_advantage( 
       export_data = export_tbl,
       pop_data = pop_tbl,
       metric = "rca"
@@ -50,26 +56,26 @@ the_plan <-
       oec_data = file_in("data/external/oec/pci_hs4_hs96_98-18.csv")
     ),
     own_rca_pci_tbl = get_pci(
-	    source = "rca",
-	    rca_data = rca_tbl
-	    ),
+      source = "rca",
+      rca_data = rca_tbl
+    ),
     own_rpca_pci_tbl = get_pci(
-	    source = "rpca",
-	    rpca_data = rpca_tbl
-	    ),
+      source = "rpca",
+      rpca_data = rpca_tbl
+    ),
     lenient_plant_pci_tbl = get_plant_complexity(
-	    output_tbl = hs96_output_tbl,
-	    rca_pci_tbl = own_rca_pci_tbl,
-	    rpca_pci_tbl = own_rpca_pci_tbl,
-	    product_match = "lenient"
-	    ),
+      output_tbl = hs96_output_tbl,
+      rca_pci_tbl = own_rca_pci_tbl,
+      rpca_pci_tbl = own_rpca_pci_tbl,
+      product_match = "lenient"
+    ),
     strict_plant_pci_tbl = get_plant_complexity(
-	    output_tbl = hs96_output_tbl,
-	    rca_pci_tbl = own_rca_pci_tbl,
-	    rpca_pci_tbl = own_rpca_pci_tbl,
-	    product_match = "strict"
-	    ),
-
+      output_tbl = hs96_output_tbl,
+      rca_pci_tbl = own_rca_pci_tbl,
+      rpca_pci_tbl = own_rpca_pci_tbl,
+      product_match = "strict"
+    ),
+    
     # Clean power data ----------------------------------------------
     energy_supply_tbl = clean_energy_supply()
     
@@ -77,10 +83,9 @@ the_plan <-
     # Perform analysis ----------------------------------------------
     # Robustness checks ---------------------------------------------
     # Create figures ------------------------------------------------
-
+    
     # TODO: Distribution of plant complexity in strict vs lenient matches
-    # TODO: 
-
-
-
+    # TODO: Distribution of plant complexity in different states
+    # TODO: Distribution of product complexity
+    
   )
